8177b2628eaad2a4cd1c5b6c80b74fb5d2a4c18d
Dara <darajsv@gmail.com>
2025-07-28 10:57:26 -0300

    adds auth tests

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -2,19 +2,18 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { passportJwtSecret } from 'jwks-rsa';
-import * as dotenv from 'dotenv';
-dotenv.config();
+import env from '@config/env';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      audience: process.env.AUTH0_AUDIENCE,
-      issuer: process.env.AUTH0_ISSUER_URL,
+      audience: env().auth0.audience,
+      issuer: env().auth0.issuerUrl,
       algorithms: ['RS256'],
       secretOrKeyProvider: passportJwtSecret({
-        jwksUri: `${process.env.AUTH0_ISSUER_URL}.well-known/jwks.json`,
+        jwksUri: `${env().auth0.issuerUrl}.well-known/jwks.json`,
         cache: true,
         rateLimit: true,
       }),
